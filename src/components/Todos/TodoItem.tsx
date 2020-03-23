a858fa9f0f8acf5f25e0601b36c74875e7feefd2
CFCode-git <435184766@qq.com>
2020-03-23 21:28:50 +0800

    完成 TodoItem

---
+++ src/components/Todos/TodoItem.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import {Checkbox} from 'antd';
+
+
+interface ITodoItemProps {
+  id: number;
+  description: string;
+  completed: boolean;
+  update: (id: number, params: any) => void
+}
+
+class TodoItem extends React.Component<ITodoItemProps> {
+  constructor(props) {
+    super(props);
+  }
+
+  update = (params: any) => {
+    this.props.update(this.props.id, params);
+  };
+
+  componentDidMount(){
+    console.log(this.props)
+  }
+
+  public render() {
+    return (
+      <div className="TodoItem" id="TodoItem">
+        <Checkbox checked={this.props.completed}
+                  onChange={e => this.update({completed: e.target.checked})}
+        >{this.props.description}</Checkbox>
+      </div>
+    );
+  }
+}
+
+export default TodoItem;
