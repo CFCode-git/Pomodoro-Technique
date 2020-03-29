6183ad24acae42a9bb749631351eb3db9fca7774
CFCode-git <435184766@qq.com>
2020-03-29 19:05:47 +0800

    完成折线统计图

--- src/components/Statistics/Statistics.tsx
+++ src/components/Statistics/Statistics.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import './Statistics.scss';
+import dayjs from 'dayjs';
+import _ from 'lodash';
+import Polygon from './Polygon';
 
 interface IStatisticsProps {
   todos: any[]
@@ -10,7 +13,13 @@ class Statistics extends React.Component<IStatisticsProps> {
   constructor(props) {super(props);}
 
   get finishedTodos() {
-    return this.props.todos.filter(t=>t.completed && !t.deleted);
+    return this.props.todos.filter(t => t.completed && !t.deleted);
+  }
+
+  get dailyTodos() {
+    return _.groupBy(this.finishedTodos, (todo) => {
+      return dayjs(todo.updated_at).format('YYYY-MM-D');
+    });
   }
 
   render() {
@@ -20,7 +29,10 @@ class Statistics extends React.Component<IStatisticsProps> {
           <li>统计</li>
           <li>目标</li>
           <li>番茄历史</li>
-          <li>累计完成 {this.finishedTodos.length} 个任务</li>
+          <li>累计完成 {this.finishedTodos.length} 个任务
+
+            <Polygon data={this.dailyTodos} totalFinishedCount={this.finishedTodos.length}/>
+          </li>
         </ul>
       </div>
     );
@@ -28,7 +40,7 @@ class Statistics extends React.Component<IStatisticsProps> {
 }
 
 const mapStateToProps = (state, ownProps) => ({
-  todos:state.todos,
+  todos: state.todos,
   ...ownProps
 });
 
